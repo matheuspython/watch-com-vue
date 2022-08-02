cf1e546ff99728f020496fd5e317117fcebee528
matheuspython <ruivorobado@gmail.com>
2022-08-02 14:07:55 -0300

    app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <TheHeader />
+  <TheMain />
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheMain from './components/TheMain.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    TheMain
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: sans-serif,Avenir, Helvetica, Arial;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    welcome
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'TheHeader'
+  }
+
+</script>
+
+<style scoped>
+  header{
+    width: 100%;
+    height: 45px;
+    background: #7f22d6;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+  }
+</style>
